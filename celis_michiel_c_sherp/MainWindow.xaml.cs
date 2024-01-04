bb92f48d6c820fca1dc98a1b969ecef36019ec11
michiel-celis-91 <michiel.celis@outlook.com>
2024-01-04 13:02:50 +0100

    COOKIE CLICKER-03 COOKIE CLICKER-04 COOKIE CLICKER-05 (lambda handler) COOKIE CLICKER-06 COOKIE CLICKER-07 (placement of counters under items preferred)

--- celis_michiel_c_sherp/MainWindow.xaml.cs
+++ celis_michiel_c_sherp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace celis_michiel_c_sherp
 {
@@ -20,9 +21,42 @@ namespace celis_michiel_c_sherp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isMouseDown = false;
         public MainWindow()
         {
             InitializeComponent();
+
+        /// Event handlers
+        /// Cookie click valid click lambda handler
+        CookieImage.MouseDown += (s, e) => { isMouseDown = true; CookieClick(); };
+        CookieImage.MouseUp += (s, e) => isMouseDown = false;
+        CookieImage.MouseEnter += (s, e) => { if (isMouseDown) CookieClick(); };
+        }
+
+        /// Event Actions
+        /// CookieClick
+        private void CookieClick()
+        {
+            AnimateClick(CookieImage);
+            CookieCounter.Text = (int.Parse(CookieCounter.Text) + 1).ToString();
+        }
+
+        /// Animations
+        private void AnimateClick(Image image)
+        {
+            var animation = new DoubleAnimation
+            {
+                From = 1.0,
+                To = 0.9,
+                Duration = new Duration(TimeSpan.FromMilliseconds(100)),
+                AutoReverse = true
+            };
+
+            var transform = new ScaleTransform();
+            image.RenderTransform = transform;
+            image.RenderTransformOrigin = new Point(0.5, 0.5);
+            transform.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+            transform.BeginAnimation(ScaleTransform.ScaleYProperty, animation);
         }
     }
 }
--- celis_michiel_c_sherp/obj/Debug/celis_michiel_c_sherp_Content.g.i.cs
+++ celis_michiel_c_sherp/obj/Debug/celis_michiel_c_sherp_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("background.jpeg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("res/cookie.png")]
 
 
